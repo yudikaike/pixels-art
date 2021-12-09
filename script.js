ea71a64a300cee965808c8e3b9a9b2add744eb33
Kaike Yudi <kaikeyudi@outlook.com>
2021-12-09 15:36:08 -0300

    refactor: define preto como primeira cor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function setColors() {
-  const colorList = ['red', 'blue', 'green', 'yellow'];
+  const colorList = ['black', 'red', 'blue', 'green'];
   const colors = document.getElementsByClassName('color');
   for (let index = 0; index < colors.length; index += 1) {
     colors[index].style.backgroundColor = colorList[index];
